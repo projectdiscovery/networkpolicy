e6d02a01d67f6753b4568be98d1aa0ba83435a01
Doğan Can Bakır <dogancanbakir@protonmail.com>
2024-03-05 21:55:34 +0300

    fix Validate func

--- go.mod
+++ go.mod
@@ -14,10 +14,16 @@ require (
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.24 // indirect
+	github.com/miekg/dns v1.1.55 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/projectdiscovery/blackrock v0.0.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20221019170559-20944726eadf // indirect
+	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- networkpolicy_test.go
+++ networkpolicy_test.go
@@ -19,3 +19,33 @@ func TestValidateAddress(t *testing.T) {
 	ok = np.ValidateAddress("192.168.1.1")
 	require.Equal(t, true, ok, "Unexpected negative result")
 }
+
+func TestMultipleCases(t *testing.T) {
+	var testCases = []struct {
+		address       string
+		expectedValid bool
+	}{
+		{"projectdiscovery.io", false},
+		{"projectdiscovery.io:80", false},
+		{"http://scanme.sh", false},
+		{"scanme.sh:8080", true},
+	}
+
+	var npOptions Options
+	npOptions.DenyList = append(npOptions.DenyList,
+		"projectdiscovery.io",
+		"projectdiscovery.io:80",
+		"http://scanm.\\.sh",
+		"honey\\.scanme\\.sh",
+	)
+
+	np, err := New(npOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, tc := range testCases {
+		ok := np.Validate(tc.address)
+		require.Equal(t, tc.expectedValid, ok, "Unexpected result for address: "+tc.address)
+	}
+}
